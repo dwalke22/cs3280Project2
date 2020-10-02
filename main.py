f038d30186f68af208da263c7ee4eac50256ebc0
dwalke22 <dwalke22@cs3280-vm14.uwg.westga.edu>
2020-10-01 21:54:20 -0400

    Set basic python script structure in main

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Script for calculating the subnet for an IP and mask
+
+Subnet Calculation
+Given an IP address (IPv4 or IPv6) and mask
+a bitwise-And calculation will be performed.
+"""
+
+__author__ = "David Walker"
+__version__ = "Fall 2020"
+
+def main():
+    """Main entry point of prgram."""
+
+if __name__ == '__main__':
+    main()
