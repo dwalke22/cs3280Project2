c7bf1d766adf76dc64d3c0bca243e1cd0a444763
dwalke22 <dwalke22@cs3280-vm10.uwg.westga.edu>
2020-10-05 21:37:25 -0400

    Wrote unittest for verify ipv4 & 6. All Pass

--- utils.py
+++ utils.py
@@ -9,13 +9,17 @@ def verify_ipv6(ip):
     if ip is None:
         raise ValueError('IP Adress cannot be None.')
     ip_regex = re.compile(r'^(?:[A-F0-9]{1,4}:){6}(?:[A-F0-9]{1,4}:[A-F0-9]{1,4}|(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))$')
+    real = False
     if ip_regex.match(ip):
-        print("Real")
+        real = True
+    return real
 
 def verify_ipv4(ip):
     """Verifies that the ip is a vaild IPv4 address"""
     if ip is None:
         raise ValueError('IP Adress cannot be None.')
-    ip_regex = re.compile(r'[25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2}\.]{3}[25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2}]')
+    ip_regex = re.compile(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+    real = False
     if ip_regex.match(ip):
-        print("Real")
+        real = True
+    return real
--- utils_tests.py
+++ utils_tests.py
@@ -10,6 +10,26 @@ __author__ = "David Walker"
 __version__ = "Fall 2020"
 
 class TestUtils(unittest.TestCase):
+    def test_none_ipv6(self):
+        self.assertRaises(ValueError, utils.verify_ipv6, None)
+
+    def test_nonvalid_ipv6(self):
+        self.assertFalse(utils.verify_ipv6("12"))
+
+    def test_valid_ipv6_stand(self):
+        self.assertTrue(utils.verify_ipv6("1762:0:0:0:0:B03:1:AF18"))
+
+    def test_valid_ipv6_mixed(self):
+        self.assertTrue(utils.verify_ipv6("1762:0:0:0:0:B03:127.32.67.15"))
+
+    def test_none_ipv4(self):
+        self.assertRaises(ValueError, utils.verify_ipv4, None)
+
+    def test_nonvalid_ipv4(self):
+        self.assertFalse(utils.verify_ipv4("12"))
+
+    def test_valid_ipv4(self):
+        self.assertTrue(utils.verify_ipv4("192.168.244.3"))
 
 if __name__ == "__main__":
     unittest.main()
