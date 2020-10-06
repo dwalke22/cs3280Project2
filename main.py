3e63f7bdfc197c902d63744912917ae00c48babb
dwalke22 <dwalke22@cs3280-vm10.uwg.westga.edu>
2020-10-05 20:45:04 -0400

    added verifying methods

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Subnet Calculation
 Given an IP address (IPv4 or IPv6) and mask
 a bitwise-And calculation will be performed.
 """
+import re
 import utils
 
 __author__ = "David Walker"
@@ -15,7 +16,13 @@ def main():
     """Main entry point of prgram."""
     ip = input("Please enter an IP Adress: ")
     net_mask = input("Please enter a subnet mask: ")
-    utils.is_valid_IP(ip)
+    letter_regex = re.compile(r'[a-zA-Z:]')
+    if letter_regex.search(ip):
+        utils.verify_ipv6(ip)
+    else:
+        utils.verify_ipv4(ip)
+
+    
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -4,7 +4,16 @@ Provides utility method for bitwises-Add
 """
 import re
 
-def is_valid_IP(ip):
+def verify_ipv6(ip):
+    """Verifies that the ip is a valid IPv6 address"""
+    if ip is None:
+        raise ValueError('IP Adress cannot be None.')
+    ip_regex = re.compile(r'^(?:[A-F0-9]{1,4}:){6}(?:[A-F0-9]{1,4}:[A-F0-9]{1,4}|(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))$')
+    if ip_regex.match(ip):
+        print("Real")
+
+def verify_ipv4(ip):
+    """Verifies that the ip is a vaild IPv4 address"""
     if ip is None:
         raise ValueError('IP Adress cannot be None.')
     ip_regex = re.compile(r'[25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2}\.]{3}[25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2}]')
